8b34aec075e7dc7d69fe8e22aa18b8a813788240
Lichyo <lichyo003@gmail.com>
2023-06-08 10:42:38 +0800

    welcome page UI finished

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton({super.key, this.title, this.color, this.onPressed});
+
+  final String? title;
+  final Color? color;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 400.0,
+          height: 42.0,
+          child: Text(
+            title!,
+            style: kButtonTextStyle.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Ubuntu',
+);
